6d4cdfbf9c3986209723c10a00ae1f2cc498aed5
Joao <suporte_carlos@live.com>
2021-07-11 01:46:17 -0300

    First upload app

---
+++ ShoppingList/ShoppingListWFA/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace ShoppingListWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ProductList = new System.Windows.Forms.DataGridView();
+            this.ShoppingList = new System.Windows.Forms.DataGridView();
+            this.btnGenerateList = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.NameProduct = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShoppingList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProductList
+            // 
+            this.ProductList.AllowUserToAddRows = false;
+            this.ProductList.AllowUserToDeleteRows = false;
+            this.ProductList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductList.Location = new System.Drawing.Point(14, 54);
+            this.ProductList.Name = "ProductList";
+            this.ProductList.ReadOnly = true;
+            this.ProductList.RowTemplate.Height = 25;
+            this.ProductList.Size = new System.Drawing.Size(455, 639);
+            this.ProductList.TabIndex = 0;
+            this.ProductList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ProductList_CellClick);
+            // 
+            // ShoppingList
+            // 
+            this.ShoppingList.AllowUserToAddRows = false;
+            this.ShoppingList.AllowUserToDeleteRows = false;
+            this.ShoppingList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShoppingList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.ShoppingList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ShoppingList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameProduct});
+            this.ShoppingList.Location = new System.Drawing.Point(731, 54);
+            this.ShoppingList.Name = "ShoppingList";
+            this.ShoppingList.ReadOnly = true;
+            this.ShoppingList.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.ShoppingList.RowTemplate.Height = 25;
+            this.ShoppingList.Size = new System.Drawing.Size(455, 639);
+            this.ShoppingList.TabIndex = 1;
+            // 
+            // btnGenerateList
+            // 
+            this.btnGenerateList.Location = new System.Drawing.Point(475, 333);
+            this.btnGenerateList.Name = "btnGenerateList";
+            this.btnGenerateList.Size = new System.Drawing.Size(249, 60);
+            this.btnGenerateList.TabIndex = 2;
+            this.btnGenerateList.Text = "Generate Shopping List";
+            this.btnGenerateList.UseVisualStyleBackColor = true;
+            this.btnGenerateList.Click += new System.EventHandler(this.btnGenerateList_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(412, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select the products you would like to add to the shopping list:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(731, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "These are the products you selected:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(475, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 45);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Click on the button to generate your shopping list:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ShoppingListWFA.Properties.Resources.shopping_cart;
+            this.pictureBox1.Location = new System.Drawing.Point(475, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ShoppingListWFA.Properties.Resources.kisspng_project_management_resource_building_project_team_list_5abf41cd08e505_3516874615224836610364;
+            this.pictureBox2.Location = new System.Drawing.Point(476, 400);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(249, 293);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // NameProduct
+            // 
+            this.NameProduct.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.NameProduct.HeaderText = "New Product List";
+            this.NameProduct.Name = "NameProduct";
+            this.NameProduct.ReadOnly = true;
+            this.NameProduct.Width = 410;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1200, 707);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerateList);
+            this.Controls.Add(this.ShoppingList);
+            this.Controls.Add(this.ProductList);
+            this.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Shopping List";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShoppingList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ProductList;
+        private System.Windows.Forms.Button btnGenerateList;
+        public System.Windows.Forms.DataGridView ShoppingList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameProduct;
+    }
+}
+
---
+++ ShoppingList/ShoppingListWFA/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ShoppingListWFA
+{
+    public partial class Form1 : Form
+    {
+        //Instância da lista que será preenchida
+        List<Model.Product> _ProductList = new List<Model.Product>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string _filePath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+
+            _filePath = Directory.GetParent(_filePath).FullName;
+
+            _filePath = Directory.GetParent(Directory.GetParent(_filePath).FullName).FullName;
+
+            _filePath += @"\ProductList\ProductList.txt";
+
+            string[] array = File.ReadAllLines(_filePath);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Model.Product product = new Model.Product();
+
+                string[] assistant = array[i].Split(' ');
+
+                product.ProductName = assistant[0];
+
+                _ProductList.Add(product);
+            }
+
+            this.ProductList.DataSource = _ProductList;
+
+            ProductList.Columns[0].HeaderText = "Product List";
+
+            ProductList.Columns[0].Width = 410;
+
+            ProductList.Refresh();
+        }
+
+        private void ProductList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (ProductList.SelectedRows.Count > 0)
+            {
+                var row = ProductList.SelectedRows[0];
+
+                string NameProduct = row.Cells[0].Value.ToString();
+
+                ShoppingList.Rows.Add(NameProduct);
+            }
+        }
+
+        private void btnGenerateList_Click(object sender, EventArgs e)
+        {
+            exportDataGridViewToTxt(ShoppingList);
+        }
+        public void exportDataGridViewToTxt(DataGridView dgv)
+        {
+            System.IO.StreamWriter streamWriter = null;
+
+            string delimiter = "\t";
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            saveFileDialog.Filter = "data file txt (*.txt)|*.txt";
+
+            saveFileDialog.FileName = "ShoppingList";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string path = saveFileDialog.FileName;
+
+                    streamWriter = new System.IO.StreamWriter(path);
+
+                    int numberOfColumns = dgv.Columns.Count;
+
+                    foreach (DataGridViewRow dgvRow in dgv.Rows)
+                    {
+                        string row = null;
+
+                        for (int i = 0; i < numberOfColumns; i++)
+                        {                            
+                            row += dgvRow.Cells[i].Value.ToString() + delimiter;
+                        }
+
+                        streamWriter.WriteLine(row);
+                    }
+
+                    MessageBox.Show("Successfully Exported", "Successfully Exported", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+}
